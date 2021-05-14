8dc9ba7ee2998acb2b393c1a0e78799da3189542
e <65376762+hdhdhfhfirjf@users.noreply.github.com>
2021-05-14 14:21:37 -0400

    Add files via upload

--- vergil.py
+++ vergil.py
@@ -1,4 +1,4 @@
-import discord, random, asyncio, json, time, string
+import discord, random, asyncio, json, time, string, re
 from discord.ext.commands import Bot
 from discord.ext import commands
 from discord import Emoji
@@ -10,6 +10,7 @@ from discord.ext import commands
 from discord import File
 import discord
 import os
+from discord.ext.commands import CommandNotFound
 
 intents = discord.Intents().all()
 bot = commands.Bot(command_prefix='!', case_insensitive=True, intents=intents)
@@ -28,15 +29,16 @@ async def on_ready():
     print(bot.user)
     print('-' * 30)
 
-    mmChannel = bot.get_channel(840582993507647498)
-    await mmChannel.purge(limit=1)
+    # mmChannel = bot.get_channel(840582993507647498)
+    # await mmChannel.purge(limit=1)
 
-    channel1 = bot.get_channel(840582993507647498)
-    embed1=discord.Embed(title=f"**Call a middleman here**", color=0x021ff7)
-    embed1.add_field(name="**!call trade details (e.g. tw octane for 100000 credits on pc)**", value=f"the command will create a private channel for your deal with a middleman, please be patient.", inline=False)
-    embed1.set_author(name="Vergil")
-    embed1.set_footer(text="e:)")
-    msgg = await channel1.send(embed=embed1)
+    # channel1 = bot.get_channel(840582993507647498)
+    # embed1=discord.Embed(title=f"**Call a middleman here**", color=0x021ff7)
+    # embed1.add_field(name="**!call trade details (e.g. tw octane for 100000 credits on pc)**", value=f"the command will create a private channel for your deal with a middleman, please be patient.", inline=False)
+    # embed1.add_field(name="**!call @user trade details (e.g. tw octane for 100000 credits on pc)**", value=f"the command will create a private channel for your deal with a middleman, please be patient.", inline=False)
+    # embed1.set_author(name="Vergil")
+    # embed1.set_footer(text="e:)")
+    # msgg = await channel1.send(embed=embed1)
         
 @bot.event
 async def on_raw_reaction_add(payload):
@@ -49,6 +51,7 @@ async def on_raw_reaction_remove(payload):
     global roleMessage
     if payload.message_id == roleMessage:
         await handle_reaction(payload, False)
+
 
 async def handle_reaction(payload, action):
     guild = bot.get_guild(payload.guild_id)
@@ -342,6 +345,21 @@ async def unmute_command(ctx, user : discord.Member):
 
 @bot.listen()
 async def on_message(message):
+    
+    with open('files/custom_commands.json', 'r') as json_file:
+        all_commands = json.load(json_file)
+
+    if all_commands and (message.content[1:] in all_commands['normal']):
+        await message.channel.send(all_commands['normal'][message.content[1:]])
+    if all_commands and (message.content[1:] in all_commands['embed']):
+        embed = discord.Embed(title="Vergil", description=f"**{all_commands['embed'][message.content[1:]]}**", color=0x1406EF)
+        await message.channel.send(embed=embed)
+    if all_commands and (message.content[1:] in all_commands['embedurls']):
+        embed = discord.Embed(title="Vergil", color=0x1406EF)
+        embed.set_image(url=all_commands['embedurls'][message.content[1:]])
+        await message.channel.send(embed=embed)
+
+
     mess = message.content.lower()
     bad_words = ["nigger", "nigga", "rape", "rapist", "pedo", "pedophile"]
 
@@ -787,8 +805,9 @@ def randomDigits():
 
 @bot.command(aliases=['cal', 'calll', 'caal'])
 async def call(ctx, *, details):
-    user = ctx.message.author.id 
+    member = ctx.author
     guild = bot.get_guild(830366087866351646)
+
 
     bsKingRole = get(guild.roles, name="Burnt Sienna King")
     everyoneRole = get(guild.roles, name="@everyone")
@@ -849,24 +868,184 @@ async def call(ctx, *, details):
     buyerChannel = f'{ctx.message.author.name}-{digits}'
     channell = await guild.create_text_channel(buyerChannel, overwrites=overwrites, category=category)
     newChannel = bot.get_channel(int(channell.id))
-    await newChannel.send(f'**Details**:\nTrade: {details}\nUser: {ctx.author.mention}\nDiscord Id: {ctx.author.id}')
-    await newChannel.send(f'{ctx.author.mention}, Please wait for a middleman.')
+    user1 = re.findall('<@!(\d{16,24})>', details)
+    if user1:
+        details = details.split('> ')
+        user0 = user1[0]
+        user1 = user1[0]
+        user1 = user1.replace('<@!', '').replace('>', '') 
+        user2 = get(bot.get_all_members(), id=int(user1))
+        await newChannel.set_permissions(member, read_messages=True)
+        await newChannel.set_permissions(user2, read_messages=True)
+        await newChannel.send(f'**Details**:\nTrade: {details[1]}\n\nUser 1: {ctx.author.mention}\nDiscord Id: {ctx.author.id}\n\nUser 2: {user2.mention}\nDiscord Id: {user2.id}')
+        await newChannel.send(f'\n{ctx.author.mention}, Please wait for a middleman.')
+    if not user1:
+        await newChannel.set_permissions(member, read_messages=True)
+        await newChannel.send(f'**Details**:\nTrade: {details}\n\nUser: {ctx.author.mention}\nDiscord Id: {ctx.author.id}')
+        await newChannel.send(f'\n{ctx.author.mention}, Please wait for a middleman.')
 
 @commands.has_any_role(*roles)
 @bot.command(aliases=['close', 'closee', 'clos', 'finish', 'finished'])
 async def done(ctx):
     await ctx.channel.delete()
 
-async def on_command_error(ctx, error):
-    if isinstance(error, commands.CommandNotFound):
-        await ctx.send("Command not found")
-    if isinstance(error, commands.MissingRequiredArgument):
-        await ctx.send("Missing a required argument. Do !help")
-    if isinstance(error, commands.MissingPermissions):
-        await ctx.send("You do not have permission to use this command")
-    if isinstance(error, commands.BotMissingPermissions):
-        await ctx.send("I don't have sufficient permissions!")
-    else:
-        pass
+@bot.command()
+async def invite(ctx):
+    invite_link = await ctx.channel.create_invite()
+    embed = discord.Embed(title='Server invite', description='send this link to invite people to this server')
+    embed.add_field(name='link:', value=invite_link, inline=False)
+    await ctx.send(embed=embed)
 
-bot.run('TOKEN')
+@bot.command()
+async def ping(ctx):
+    msg = await ctx.send(f"**pinging...**")
+    await msg.edit(content=f"**:stopwatch:  _{round(bot.latency * 1000)}_ ms**")
+
+@commands.has_any_role(*roles)
+@bot.command(aliases=['new-command', 'add-command'])
+async def add_command(ctx, command_name, *, command_reply):
+    if (command_name is None) or (not command_reply):
+        await ctx.send('Missing Arguments')
+    
+    with open('files/custom_commands.json', 'r') as json_file:
+        all_commands = json.load(json_file)
+
+    if "-embed" in command_reply:
+        command_reply = command_reply.split(' -embed')
+
+        all_commands['embed'][command_name] = command_reply[0]
+    
+        with open('files/custom_commands.json', 'w') as json_file:
+            json.dump(all_commands, json_file)
+
+    elif "-url" in command_reply:
+        command_reply = command_reply.split(' -url')
+
+        all_commands['embedurls'][command_name] = command_reply[0]
+    
+        with open('files/custom_commands.json', 'w') as json_file:
+            json.dump(all_commands, json_file)
+    else:
+        all_commands[command_name] = command_reply
+        
+        with open('files/custom_commands.json', 'w') as json_file:
+            json.dump(all_commands, json_file)
+
+    timestamp = time.strftime('%H:%M:%S')
+    embed = discord.Embed(title="Vergil", colour=discord.Colour(0x1406EF))
+    embed.set_footer(text=f"e:) | {timestamp}", icon_url=ctx.author.avatar_url)
+    embed.set_thumbnail(url=ctx.author.avatar_url)
+    embed.add_field(name=f"**{command_name} has been added**", value=f"**By {ctx.author.name}**", inline=False)
+    mess = await ctx.send(embed=embed)
+    await asyncio.sleep(3)
+    await mess.delete()
+    await ctx.message.delete()
+    logChan = bot.get_channel(840598868269072404)
+    await logChan.send(embed=embed)
+
+@commands.has_any_role(*roles)
+@bot.command(aliases=['delete-command', 'del-command'])
+async def delete_command(ctx, command_name):
+
+    with open('files/custom_commands.json', 'r') as json_file:
+        all_commands = json.load(json_file)
+    
+    if command_name in all_commands['embed']:
+        all_commands['embed'].pop(command_name)
+
+    elif command_name in all_commands['embedurls']:
+        all_commands['embedurls'].pop(command_name)
+    
+    elif command_name in all_commands['normal']:
+        all_commands['normal'].pop(command_name)
+    
+    else:
+        await ctx.send(f'**{command_name} not found**')
+        return
+
+
+    with open('files/custom_commands.json', 'w') as json_file:
+        json.dump(all_commands, json_file)
+    
+    await ctx.send(f'**{command_name} has been deleted successfully**')
+
+@bot.command(aliases=['custom-commands'])
+async def custom_commands(ctx):
+
+    with open('files/custom_commands.json', 'r') as json_file:
+        all_servers = json.load(json_file)
+
+
+    if all_servers:
+        embed = discord.Embed(title="Custom Commands", description="here are the server's custom commands: ")
+        for command in all_servers['normal'].keys():
+            embed.add_field(name=f'{command} [normal]', value=all_servers['normal'][command], inline=False)
+            
+        for command in all_servers['embed'].keys():
+            embed.add_field(name=f'{command} [embedded]', value=all_servers['embed'][command], inline=False)
+        for command in all_servers['embedurls'].keys():
+            embed.add_field(name=f'{command} [embedded picture]', value=all_servers['embedurls'][command], inline=False)
+    else:
+        embed = discord.Embed(title="No custom commands have been found",
+                                description=f"type `add-command` to add first command")
+
+    await ctx.send(embed=embed)
+
+@bot.event
+async def on_command_error(ctx, error):
+    if hasattr(ctx.command, 'on_error'):
+            return
+            
+    error = getattr(error, 'original', error)
+
+    if isinstance(error, commands.CommandNotFound):
+        return
+
+    if isinstance(error, commands.BotMissingPermissions):
+        missing = [perm.replace('_', ' ').replace('guild', 'server').title() for perm in error.missing_perms]
+        if len(missing) > 2:
+            fmt = '{}, and {}'.format("**, **".join(missing[:-1]), missing[-1])
+        else:
+            fmt = ' and '.join(missing)
+        _message = 'I need the **{}** permission(s) to run this command.'.format(fmt)
+        await ctx.send(_message)
+        return
+
+    if isinstance(error, commands.DisabledCommand):
+        await ctx.send('This command has been disabled.')
+        return
+
+    if isinstance(error, commands.CommandOnCooldown):
+        await ctx.send("This command is on cooldown, please retry in {}s.".format(math.ceil(error.retry_after)))
+        return
+
+    if isinstance(error, commands.MissingPermissions):
+        missing = [perm.replace('_', ' ').replace('guild', 'server').title() for perm in error.missing_perms]
+        if len(missing) > 2:
+            fmt = '{}, and {}'.format("**, **".join(missing[:-1]), missing[-1])
+        else:
+            fmt = ' and '.join(missing)
+        _message = 'You need the **{}** permission(s) to use this command.'.format(fmt)
+        await ctx.send(_message)
+        return
+
+    if isinstance(error, commands.UserInputError):
+        await ctx.send("Invalid input.")
+        await ctx.send("!help")
+        return
+
+    if isinstance(error, commands.NoPrivateMessage):
+        try:
+            await ctx.author.send('This command cannot be used in direct messages.')
+        except discord.Forbidden:
+            pass
+        return
+
+    if isinstance(error, commands.CheckFailure):
+        await ctx.send("You do not have permission to use this command.")
+        return
+
+
+
+
+bot.run(TOKEN)
